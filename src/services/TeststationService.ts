2c73bb4fcc1a4ee955281720e69700dacf525f76
Max Kiehnscherf <max.kiehnscherf@gmail.com>
2020-03-21 17:11:18 +0100

    [feat] implement cr-d routes for teststations

--- src/services/TeststationService.ts
+++ src/services/TeststationService.ts
@@ -1,12 +1,27 @@
 import Teststation, { ITeststation } from "../entities/Teststation"
+import { v4 as UUID } from "uuid"
 
 export class TeststationService {
   public static create(station: ITeststation): Promise<Teststation> {
-    return Teststation.create(station)
+    const insertPayload: ITeststation = {
+      id: UUID(),
+      ...station
+    }
+    return Teststation.create(insertPayload)
   }
 
   public static find(id: string): Promise<Teststation | null> {
     return Teststation.findByPk(id)
+  }
+
+  public static async delete(id: string): Promise<void> {
+    const maybeStation = await Teststation.findByPk(id)
+
+    if(maybeStation) {
+      return maybeStation.destroy()
+    } else {
+      return Promise.resolve()
+    }
   }
 
   public static findAll(): Promise<Teststation[]> {
